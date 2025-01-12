dd65fb931ecf5389568e484bd2652fe7ccd8c996
bharani_bbk <bharani.bbk.dev@gmail.com>
2025-01-12 22:26:32 +0530

    update game details layout

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,7 +3,7 @@ import GameScreenshots from "@/components/GameScreenshots";
 import GameTrailer from "@/components/GameTrailer";
 import TextSummorize from "@/components/TextSummorize";
 import useGame from "@/hooks/useGame";
-import { Heading, Spinner } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 
 const GameDetailPage = () => {
@@ -14,15 +14,25 @@ const GameDetailPage = () => {
   if (error || !data) throw error;
 
   return (
-    <>
-      <Heading marginBottom={4} fontSize="4xl">
-        {data.name}
-      </Heading>
-      <TextSummorize>{data.description_raw}</TextSummorize>
-      <GameAttributes game={data} />
-      <GameTrailer gameId={data.id} />
-      <GameScreenshots gameId={data.id} />
-    </>
+    <SimpleGrid
+      columns={{
+        base: 1,
+        md: 2,
+      }}
+      gap={5}
+    >
+      <GridItem>
+        <Heading marginBottom={4} fontSize="4xl">
+          {data.name}
+        </Heading>
+        <TextSummorize>{data.description_raw}</TextSummorize>
+        <GameAttributes game={data} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={data.id} />
+        <GameScreenshots gameId={data.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
